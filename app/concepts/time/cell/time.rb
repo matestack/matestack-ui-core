a37721b22d98eeb06270adf659b45e958e24ecc5
p_doub <pascalwengerter@gmail.com>
2019-03-07 08:56:37 +0100

    re-name concept

--- app/concepts/time/cell/time.rb
+++ app/concepts/time/cell/time.rb
@@ -1,10 +1,10 @@
-module Timefield::Cell
-  class Timefield < Component::Cell::Static
+module Time::Cell
+  class Time < Component::Cell::Static
 
     def setup
       @tag_attributes.merge!({ "class": options[:class],
         "id": component_id,
-        "datetime": options[:time] ||= nil
+        "datetime": options[:datetime] ||= nil
       })
     end
 
