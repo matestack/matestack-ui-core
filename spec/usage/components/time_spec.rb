1a1ce6f4ca856684f53b74d4f5b4f89aaa4000f4
p_doub <pascalwengerter@gmail.com>
2019-05-02 10:20:01 +0200

    Fix failing test for time component

--- spec/usage/components/time_spec.rb
+++ spec/usage/components/time_spec.rb
@@ -11,17 +11,17 @@ describe 'Time Component', type: :feature, js: true do
         components {
           # simple time tag
           pg do
-            plain 'This should show '
-            time class: 'my-simple-time' do
-              plain '12:00'
+            plain "This should show "
+            time class: "my-simple-time" do
+              plain "12:00"
             end
           end
 
           # time tag with timestamp
-          pg id: 'my-parent-paragraph' do
-            plain 'Today is '
-            time id: 'example-timestamp', datetime: DateTime.new(2019,2,12,10,38,39,'+02:00') do
-              plain 'July 7'
+          pg id: "my-parent-paragraph" do
+            plain "Today is "
+            time id: "example-timestamp", datetime: DateTime.new(2019,2,12,10,38,39,'+02:00') do
+              plain "July 7"
             end
           end
         }
@@ -34,11 +34,11 @@ describe 'Time Component', type: :feature, js: true do
     static_output = page.html
 
     expected_static_output = <<~HTML
-    <p>This should show <time class='my-simple-time'>12:00</time></p>
+    <p>This should show <time class="my-simple-time">12:00</time></p>
 
-    <p id='my-parent-paragraph'>
-      Today is <time datetime='2019-02-12T10:38:39+02:00' id='example-timestamp'>July 7</time>
-    </p>"
+    <p id="my-parent-paragraph">
+      Today is <time datetime="2019-02-12T10:38:39+02:00" id="example-timestamp">July 7</time>
+    </p>
     HTML
 
     expect(stripped(static_output)).to include(stripped(expected_static_output))
