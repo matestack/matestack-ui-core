70652e4181763f7b21d8073b305f00ba1f3f90fd
p_doub <pascal@wengerter.info>
2019-05-09 18:00:19 +0200

    add broken transition test

--- spec/usage/components/transition_spec.rb
+++ spec/usage/components/transition_spec.rb
@@ -113,4 +113,97 @@ describe "Transition Component", type: :feature, js: true do
     expect(first_content_on_page_1).not_to eq(refreshed_content_on_page_1)
   end
 
+  # supposed to work, but doesn't. Suspect Vue is the culprint here
+  # it "Example 2 - Perform transition from one page to another by providing route as string (not recommended)" do
+  #
+  #   class Apps::ExampleApp < App::Cell::App
+  #
+  #     def response
+  #       components {
+  #         heading size: 1, text: "My Example App Layout"
+  #         nav do
+  #           transition path: "http://127.0.0.1:35553/my_example_app/page1" do
+  #             button text: "Page 1"
+  #           end
+  #           transition path: "http://127.0.0.1:35553/my_example_app/page2" do
+  #             button text: "Page 2"
+  #           end
+  #         end
+  #         main do
+  #           page_content
+  #         end
+  #       }
+  #     end
+  #
+  #   end
+  #
+  #   module Pages::ExampleApp
+  #
+  #   end
+  #
+  #   class Pages::ExampleApp::ExamplePage < Page::Cell::Page
+  #
+  #     def response
+  #       components {
+  #         div id: "my-div-on-page-1" do
+  #           heading size: 2, text: "This is Page 1"
+  #           plain "#{DateTime.now.strftime('%Q')}"
+  #         end
+  #       }
+  #     end
+  #
+  #   end
+  #
+  #   class Pages::ExampleApp::SecondExamplePage < Page::Cell::Page
+  #
+  #     def response
+  #       components {
+  #         div id: "my-div-on-page-2" do
+  #           heading size: 2, text: "This is Page 2"
+  #           plain "#{DateTime.now.strftime('%Q')}"
+  #         end
+  #         transition path: 'my_example_app/page1' do
+  #           button text: "Back to Page 1"
+  #         end
+  #       }
+  #     end
+  #
+  #   end
+  #
+  #   class ExampleAppPagesController < ExampleController
+  #     include Matestack::Ui::Core::ApplicationHelper
+  #
+  #     def page1
+  #       responder_for(Pages::ExampleApp::ExamplePage)
+  #     end
+  #
+  #     def page2
+  #       responder_for(Pages::ExampleApp::SecondExamplePage)
+  #     end
+  #
+  #   end
+  #
+  #   Rails.application.routes.append do
+  #     get 'my_example_app/page1', to: 'example_app_pages#page1', as: 'page1_string'
+  #     get 'my_example_app/page2', to: 'example_app_pages#page2', as: 'page2_string'
+  #   end
+  #   Rails.application.reload_routes!
+  #
+  #   visit "/my_example_app/page1"
+  #
+  #   expect(page).to have_content("My Example App Layout")
+  #   expect(page).to have_button("Page 1")
+  #   expect(page).to have_button("Page 2")
+  #
+  #   expect(page).to have_content("This is Page 1")
+  #   expect(page).not_to have_content("This is Page 2")
+  #
+  #   click_button("Page 2")
+  #
+  #   expect(page).to have_content("My Example App Layout")
+  #   expect(page).not_to have_content("This is Page 1")
+  #   expect(page).to have_content("This is Page 2")
+  #
+  # end
+
 end
