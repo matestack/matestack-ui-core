6cc6d22a1a8be718dd06e00c0f69e7a9a39e00e9
Jonas Jabari <jonas@basemate.com>
2019-07-17 11:50:43 +0200

    updated js

--- vendor/assets/javascripts/matestack-ui-core.js
+++ vendor/assets/javascripts/matestack-ui-core.js
@@ -11077,11 +11077,11 @@ const componentMixin = {
       self.params["component_key"] = self.componentConfig["component_key"];
       __WEBPACK_IMPORTED_MODULE_1_axios___default()({
         method: "get",
-        url: location.pathname + location.search,
+        url: self.componentConfig["origin_url"],
         headers: {
           'X-CSRF-Token': document.getElementsByName("csrf-token")[0].getAttribute('content')
         },
-        params: { "component_key": self.componentConfig["component_key"] }
+        params: self.params
       }).then(function (response) {
         self.asyncTemplate = response["data"];
       });
