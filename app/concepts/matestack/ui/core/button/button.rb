9c6c634c5f780b556d3ebc578b6142158c808719
p_doub <pascal@wengerter.info>
2019-07-23 20:13:50 +0200

    add button docs and add disabled functionality

--- app/concepts/matestack/ui/core/button/button.rb
+++ app/concepts/matestack/ui/core/button/button.rb
@@ -1,5 +1,11 @@
 module Matestack::Ui::Core::Button
   class Button < Matestack::Ui::Core::Component::Static
 
+    def setup
+      @tag_attributes.merge!({
+        "disabled": options[:disabled] ||= nil
+      })
+    end
+
   end
 end
