f8fd72b0dddb612b240df791f1d2be7352c6f20c
p_doub <pascal@wengerter.info>
2019-07-23 19:29:42 +0200

    random commit to trigger circleCi

--- app/concepts/matestack/ui/core/component/component.js
+++ app/concepts/matestack/ui/core/component/component.js
@@ -1,7 +1,6 @@
 import Vue from 'vue/dist/vue.esm'
 import axios from 'axios'
 import VRuntimeTemplate from "v-runtime-template"
-
 import matestackEventHub from 'js/event-hub'
 
 const componentMixin = {
