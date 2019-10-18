a57f4428996c441ad42bfe0d21595b2c9bbbc257
Bryan Leighton <bryan.leigh77@gmail.com>
2019-10-18 17:10:10 -0500

    update fieldset component for disabled attribute

--- app/concepts/matestack/ui/core/fieldset/fieldset.rb
+++ app/concepts/matestack/ui/core/fieldset/fieldset.rb
@@ -1,4 +1,9 @@
 module Matestack::Ui::Core::Fieldset
   class Fieldset < Matestack::Ui::Core::Component::Static
+    def setup
+      @tag_attributes.merge!({
+        disabled: options[:disabled]
+      })
+    end
   end
 end
--- spec/usage/components/fieldset_spec.rb
+++ spec/usage/components/fieldset_spec.rb
@@ -10,10 +10,16 @@ describe 'Fieldset Component', type: :feature, js: true do
             legend text: 'input legend'
             input
           end
-
+          
           # advanced
           fieldset class: 'foo', id: 'world' do
             legend id: 'bar', class: 'hello', text: 'input legend'
+            input
+          end
+          
+          # with disabled 
+          fieldset disabled: true do
+            legend text: 'input legend'
             input
           end
         }
@@ -25,17 +31,22 @@ describe 'Fieldset Component', type: :feature, js: true do
     static_output = page.html
     
     expected_html_output = <<~HTML
-      <fieldset >
+      <fieldset>
+        <legend>input legend</legend>
+        <input>
+      </fieldset>
+      
+      <fieldset id="world" class="foo">
+      <legend id="bar" class="hello">input legend</legend>
+      <input>
+      </fieldset>
+      
+      <fieldset disabled="disabled">
         <legend>input legend</legend>
         <input>
       </fieldset>
 
-      <fieldset id="world" class="foo">
-        <legend id="bar" class="hello">input legend</legend>
-        <input>
-      </fieldset>
     HTML
-    
     expect(stripped(static_output)).to include(stripped(expected_html_output))
   end
 end
