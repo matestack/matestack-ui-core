5a2c3b0e2f5f20dc57dbb3de758981a8d7267c69
Marco Roth <marco.roth@intergga.ch>
2019-10-19 00:04:21 +0200

    refactor and improve app generator
    - change name: matestack_app => matestack:app
    - simplify the generation of an app
    - change structure of templates

--- spec/lib/generators/matestack/app/app_generator_spec.rb
+++ spec/lib/generators/matestack/app/app_generator_spec.rb
@@ -1,9 +1,9 @@
 require 'generator_spec'
-require 'generators/matestack_app/matestack_app_generator'
+require 'generators/matestack/app/app_generator'
 
-describe MatestackAppGenerator, type: :generator do
-  let(:dummy) {File.expand_path(File.join(__FILE__, '..', '..', '..', 'dummy'))}
-  let(:dummy_copy) {File.expand_path(File.join(__FILE__, '..', '..', '..', 'dummy_copy'))}
+describe Matestack::Generators::AppGenerator, type: :generator do
+  let(:dummy) { File.expand_path(File.join(__FILE__, '..', '..', '..', '..', '..', 'dummy')) }
+  let(:dummy_copy) { File.expand_path(File.join(__FILE__, '..', '..', '..', '..', '..', 'dummy_copy')) }
 
   before :each do
     FileUtils.cp_r dummy, dummy_copy
@@ -27,9 +27,9 @@ describe MatestackAppGenerator, type: :generator do
     run_generator %w(my_example_app --all_inclusive)
 
     assert_file "app/matestack/apps/my_example_app.rb", /class Apps::MyExampleApp < Matestack::Ui::App\b/
-    assert_file "app/matestack/pages/my_example_app/example_page.rb", /class Pages::MyExampleApp::ExamplePage < Matestack::Ui::Page\b/
     assert_file "app/controllers/my_example_app_controller.rb", /class MyExampleAppController < ApplicationController\b/
     assert_file "config/routes.rb", /my_example_app\b/
+    assert_file "app/matestack/pages/my_example_app/example_page.rb", /class Pages::MyExampleApp::ExamplePage < Matestack::Ui::Page\b/
   end
 
 end
