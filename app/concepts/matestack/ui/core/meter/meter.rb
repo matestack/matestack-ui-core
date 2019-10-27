69265399d6793e8c36631c7c0c7524691d0eaa18
bryan leighton <bryan.leigh77@gmail.com>
2019-10-26 20:58:00 -0500

    Add optimum, high, low optional values

--- app/concepts/matestack/ui/core/meter/meter.rb
+++ app/concepts/matestack/ui/core/meter/meter.rb
@@ -1,4 +1,14 @@
 module Matestack::Ui::Core::Meter
   class Meter < Matestack::Ui::Core::Component::Static
+    def setup
+      @tag_attributes.merge!({
+        value: options[:value],
+        min: options[:min],
+        max: options[:max],
+        low: options[:low],
+        high: options[:high],
+        optimum: options[:optimum]
+      })
+    end
   end
 end
