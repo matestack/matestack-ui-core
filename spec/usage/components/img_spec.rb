b711543d819d04163bde919844792c975c21f891
Pascal <pascal@wengerter.info>
2019-11-05 18:57:49 +0100

    Add usemap attribute to image component

--- spec/usage/components/img_spec.rb
+++ spec/usage/components/img_spec.rb
@@ -8,7 +8,10 @@ describe 'Img Component', type: :feature, js: true do
     class ExamplePage < Matestack::Ui::Page
       def response
         components {
+          # simple image
           img path: 'matestack-logo.png', width: 500, height: 300, alt: "logo"
+          # using usemap
+          img path: 'matestack-logo.png', width: 500, height: 300, alt: "otherlogo",  usemap: "#newmap"
         }
       end
     end
@@ -16,5 +19,6 @@ describe 'Img Component', type: :feature, js: true do
     visit '/example'
 
     expect(page).to have_xpath("//img[contains(@src,'matestack-logo') and @alt='logo' and @width='500' and @height='300']")
+    expect(page).to have_xpath("//img[contains(@src,'matestack-logo') and @alt='otherlogo' and @width='500' and @height='300'] and @usemap='\#newmap'")
   end
 end
