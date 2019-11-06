49306eebd952e64618dab09725148cb4c8a50c8a
Pascal <pascal@wengerter.info>
2019-11-06 13:53:51 +0100

    Fix map spec & remove p in test output

--- spec/usage/components/map_spec.rb
+++ spec/usage/components/map_spec.rb
@@ -12,8 +12,8 @@ describe 'Map Component', type: :feature, js: true do
 
           map name: 'newmap' do
             area shape: 'rect', coords: [0,0,100,100], href: 'first.htm', alt: 'First'
-            area shape: 'rect', coords: [0,0,100,100], href: 'second.htm', alt: 'Second'
-            area shape: 'rect', coords: [0,0,100,100], href: 'third.htm', alt: 'Third'
+            area shape: 'rect', coords: [100,100,200,200], href: 'second.htm', alt: 'Second'
+            area shape: 'rect', coords: [200,200,300,300], href: 'third.htm', alt: 'Third'
           end
         }
       end
@@ -24,15 +24,14 @@ describe 'Map Component', type: :feature, js: true do
     static_output = page.html
 
     expected_static_output = <<~HTML
-    <img src="matestack-logo.png" alt="otherlogo" width="500" height="300" usemap="#newmap">
-
     <map name="newmap">
-       <area shape="rect" coords="0,0,100,100" href="first.htm" alt="First">
-       <area shape="rect" coords="100,100,200,200" href="second.htm" alt="Second">
-       <area shape="rect" coords="200,200,300,300" href="third.htm" alt="Third">
+       <area alt="First" coords="0,0,100,100" href="first.htm" shape="rect">
+       <area alt="Second" coords="100,100,200,200" href="second.htm" shape="rect">
+       <area alt="Third" coords="200,200,300,300" href="third.htm" shape="rect">
     </map>
     HTML
 
     expect(stripped(static_output)).to include(stripped(expected_static_output))
+    expect(page).to have_xpath("//img[contains(@src,'matestack-logo') and @alt='otherlogo' and @width='500' and @height='300' and @usemap='\#newmap']")
   end
 end
