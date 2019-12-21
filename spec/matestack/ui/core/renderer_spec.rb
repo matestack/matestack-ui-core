201122ee13a89748af384f497757e3a32da6f0af
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-12-21 15:22:25 +0100

    adding specs for implicit page rendering for `index` and `show` actions
    https://github.com/basemate/matestack-ui-core/pull/302#issue-341812064

---
+++ spec/matestack/ui/core/renderer_spec.rb
@@ -0,0 +1,98 @@
+describe Matestack::Ui::Core::Render do
+  describe "#default_render" do
+
+    describe "implicit rendering for index actions", type: :feature, js: true do
+      before do
+        module Clients
+        end
+
+        class Clients::BookingsController < ApplicationController
+          include Matestack::Ui::Core::ApplicationHelper
+
+          def index
+          end
+        end
+
+        Rails.application.routes.draw do
+          namespace :clients do
+            resources :bookings
+          end
+        end
+
+        class Apps::Clients < Matestack::Ui::App
+          def response
+            components {
+              page_content
+            }
+          end
+        end
+
+        module Pages::Clients
+        end
+
+        class Pages::Clients::Bookings < Matestack::Ui::Page
+          def response
+            components {
+              plain "Hello from the Pages::Clients::Bookings page."
+            }
+          end
+        end
+      end
+      after do
+        Rails.application.reload_routes!
+      end
+      specify do
+        visit "/clients/bookings"
+        expect(page).to have_text "Hello from the Pages::Clients::Bookings page."
+      end
+    end
+
+    describe "implicit rendering for show actions", type: :feature, js: true do
+      before do
+        module Clients
+        end
+
+        class Clients::BookingsController < ApplicationController
+          include Matestack::Ui::Core::ApplicationHelper
+
+          def show
+            @id = params[:id]
+          end
+        end
+
+        Rails.application.routes.draw do
+          namespace :clients do
+            resources :bookings
+          end
+        end
+
+        class Apps::Clients < Matestack::Ui::App
+          def response
+            components {
+              page_content
+            }
+          end
+        end
+
+        module Pages::Clients
+        end
+
+        class Pages::Clients::Booking < Matestack::Ui::Page  # singular name `Booking`!
+          def response
+            components {
+              plain "Hello from the Pages::Clients::Booking page with id #{@id}."
+            }
+          end
+        end
+      end
+      after do
+        Rails.application.reload_routes!
+      end
+      specify do
+        visit "/clients/bookings/123"
+        expect(page).to have_text "Hello from the Pages::Clients::Booking page with id 123."
+      end
+    end
+
+  end
+end
