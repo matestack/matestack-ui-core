5b4c421dc88fadeb29e9a1d4b871b1412e8f854a
Tobias Pfeiffer <pragtob@gmail.com>
2020-01-28 11:00:19 +0100

    Custom SimpleCov configuration
    This isn't really a rails app so in parts it was doing a little
    disservice.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,19 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
-SimpleCov.start 'rails' do
+SimpleCov.start do
   enable_coverage :branch
+
+  add_filter "/spec"
+  add_filter %r{^/config/}
+
+
+  add_group "Concepts", "/app/concepts"
+  add_group "Helpers", "/app/helpers"
+  add_group "App Lib", "/app/lib"
+  add_group "Lib", %r{^/lib/}
+
+  track_files "{app,lib}/**/*.rb"
 end
 
 require 'webmock/rspec'
