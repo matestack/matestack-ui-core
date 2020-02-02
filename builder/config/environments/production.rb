d5bfe0aeed69bb46b5e4ab233ed2bd1d0fb79b18
Sebastian Fiedlschuster <git@fiedlschuster.de>
2020-02-02 16:44:59 +0100

    re-create builder app
    https://github.com/matestack/matestack-ui-core/issues/343#issuecomment-581146487

--- builder/config/environments/production.rb
+++ builder/config/environments/production.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -39,9 +37,6 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -90,7 +85,4 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 end
--- builder/config/webpack/production.js
+++ builder/config/webpack/production.js
@@ -1,16 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const merge = require('webpack-merge')
 const environment = require('./environment')
 
-const customConfig = {
-  output: {
-    filename: 'matestack-ui-core.min.js',
-    libraryTarget: 'var',
-    library: 'MatestackUiCore'
-  }
-};
-
-const config = environment.toWebpackConfig();
-
-module.exports = merge(environment.toWebpackConfig(), config, customConfig)
+module.exports = environment.toWebpackConfig()
