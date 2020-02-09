183ce150e6e04e2e7daac4f17f80bf35e5d9d049
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-02-10 00:37:23 +0100

    Configure babel to include our js files for es5 transpiling
    https://github.com/matestack/matestack-ui-core/issues/238#issuecomment-583907810

--- builder/config/webpack/production.js
+++ builder/config/webpack/production.js
@@ -12,5 +12,15 @@ environment.config.set("output.filename", chunkData => {
 const miniCssExtractPlugin = environment.plugins.get('MiniCssExtract')
 miniCssExtractPlugin.options.filename = "[name].min.css"
 
+// In order to have babel's es5 (ie11) transpiling be applied to all of the
+// js code, it needs to be included in the loader paths.
+// https://github.com/rails/webpacker/blob/master/docs/webpack.md
+// https://github.com/matestack/matestack-ui-core/issues/238
+//
+const babelLoader = environment.loaders.get('babel')
+delete babelLoader.exclude
+const path = require('path')
+babelLoader.include.push(path.resolve(__dirname, '../../../node_modules'))
+babelLoader.include.push(path.resolve(__dirname, '../../../app/concepts/matestack/ui/core'))
 
 module.exports = environment.toWebpackConfig()
