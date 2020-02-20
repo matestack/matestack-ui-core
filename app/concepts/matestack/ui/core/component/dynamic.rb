f5418f8b0adb95099a8dd5c120ea614a931e1723
Tobias Pfeiffer <pragtob@gmail.com>
2020-02-20 15:41:38 +0100

    Re-establish legacy generate_(vue_)_component_name
    (copy & paste from develop)

--- app/concepts/matestack/ui/core/component/dynamic.rb
+++ app/concepts/matestack/ui/core/component/dynamic.rb
@@ -1,9 +1,16 @@
 module Matestack::Ui::Core::Component
   class Dynamic < Base
+
+    def initialize(*_args)
+      super
+      generate_component_name
+    end
+
     def show
       render :dynamic_without_rerender
     end
 
+    private
     def dynamic_tag_attributes
       attrs = {
         "is": @component_class,
@@ -13,8 +20,44 @@ module Matestack::Ui::Core::Component
         "inline-template": true,
       }
       attrs.merge!(options[:attributes]) unless options[:attributes].nil?
-      p attrs
       attrs
-   end
+    end
+
+    # TODO: this is also basically legacy/magically reliant on naming
+    # scheme with the same custom name prefix.
+    # We might want to make that more explicit and/or also reliant on the
+    # registry registered name.
+    # Which wouldn't work anymore with add_child ClassName
+    # Can we just have this be the whole module name or you have to
+    # explicitly name it?
+    def generate_component_name
+      name_parts = self.class.name.split("::")
+      module_name = name_parts[0]
+      if module_name == "Components"
+        name_parts[0] = "Custom"
+      end
+      if name_parts.count > 1
+        if name_parts.include?("Cell")
+          name = name_parts[0] + name_parts[1]
+          if name_parts[0] == name_parts[2]
+            name = name_parts[0] + name_parts[1]
+            @component_class =  name.underscore.gsub("_", "-")
+          else
+            name = name_parts[0] + name_parts[2] + name_parts[1]
+            @component_class = name.underscore.gsub("_", "-")
+          end
+        else
+          if name_parts[-2] == name_parts[-1]
+            @component_class = name_parts[0..-2].join("-").downcase
+          else
+            @component_class = name_parts.join("-").downcase
+          end
+        end
+      else
+        name = name_parts[0]
+        @component_class = name.underscore.gsub("_", "-")
+      end
+      @component_name = @component_class
+    end
   end
 end
