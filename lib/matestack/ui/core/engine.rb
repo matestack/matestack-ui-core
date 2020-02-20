9bd94b6940d0fe909f69a8bc6bc53ad89e751795
Tobias Pfeiffer <pragtob@gmail.com>
2020-02-20 18:06:22 +0100

    Sketch of auto requiring a central register_components file in rails
    Tested it quickly with the dummy app and seemed to work.
    
    Should probably make name configurable, definitely needs to
    be documented and maybe a better name :)

--- lib/matestack/ui/core/engine.rb
+++ lib/matestack/ui/core/engine.rb
@@ -10,6 +10,16 @@ module Matestack
           # end
           # Rails.configuration.autoload_paths << Dir.glob(File.join(Rails.root, 'config/matestack/**/*.rb'))
           # require_dependency "#{Rails.root}/config/matestack-ui-core"
+
+          components_path = "#{Rails.root}/app/matestack/register_components"
+          if File.exist?("#{components_path}.rb")
+            # the other dependencies need to be loaded otherwise require_dependency crashes silently
+            # we don't need to require_dependency them because they don't need reloading
+            # anyhow, they should probably be required somewhere else before this anyhow?
+            require 'matestack/ui/core/dsl'
+            require 'matestack/ui/core/component/registry'
+            require_dependency "#{Rails.root}/app/matestack/register_components"
+          end
         end
 
         config.to_prepare &method(:activate).to_proc
