59197f206d541efdd453c87daded9d1f90239d8f
Jonas Jabari <jonas@basemate.com>
2020-03-10 17:21:45 +0100

    refactored turbolinks spec in order to show turbolinks issues

--- spec/usage/integrations/turbolinks_integration_spec.rb
+++ spec/usage/integrations/turbolinks_integration_spec.rb
@@ -1,42 +1,145 @@
 describe "Turbolinks integration", type: :feature, js: true do
-  before(:all) do
-    FileUtils.cp('spec/usage/application_with_turbolinks.js', 'spec/dummy/app/assets/javascripts/application.js')
+
+  class TestController < ActionController::Base
+
+    before_action :check_params
+
+    def check_params
+      expect_params(params.permit!.to_h)
+    end
+
+    def expect_params(params)
+    end
+
   end
 
-  before do
-    Rails.application.routes.draw do
-      get '/turbolinks', to: 'turbolinks#my_action', as: 'turbolinks_test_action'
+  it "Matestack can be used with turbolinks" do
+
+    module Apps
     end
 
-    class TurbolinksController < ActionController::Base
-      layout "application"
-
-      include Matestack::Ui::Core::ApplicationHelper
-
-      def my_action
-      end
-    end
-
-    module Pages::Turbolinks
-    end
-
-    class Pages::Turbolinks::MyAction < Matestack::Ui::Page
+    class Apps::TurbolinksTest < Matestack::Ui::App
       def response
         components {
-          plain "Hello from matestack with turbolinks"
+          nav do
+            link path: :turbolinks1_path do
+              button text: "Link to Page 1"
+            end
+            transition path: :turbolinks2_path do
+              button text: "Transition to Page 2"
+            end
+            link path: :turbolinks3_path do
+              button text: "Link to Page 3"
+            end
+          end
+          main do
+            page_content
+          end
         }
       end
     end
-  end
 
-  after do
-    FileUtils.cp('spec/usage/application.js', 'spec/dummy/app/assets/javascripts/application.js')
+    module Pages::TurbolinksTest
+    end
+
+    class Pages::TurbolinksTest::Page1 < Matestack::Ui::Page
+      def response
+        components {
+          plain "Hello from matestack with turbolinks - Page 1"
+        }
+      end
+    end
+    class Pages::TurbolinksTest::Page2 < Matestack::Ui::Page
+      def response
+        components {
+          plain "Hello from matestack with turbolinks - Page 2"
+        }
+      end
+    end
+    class Pages::TurbolinksTest::Page3 < Matestack::Ui::Page
+      def response
+        components {
+          plain "Hello from matestack with turbolinks - Page 3"
+          action action_config do
+            button text: "click me"
+          end
+        }
+      end
+
+      def action_config
+        return {
+          method: :post,
+          path: :action_test_path,
+          data: {
+            foo: "bar"
+          }
+        }
+      end
+    end
+
+    Rails.application.routes.append do
+      get '/turbolinks1', to: 'turbolinks_test#page1', as: :turbolinks1
+      get '/turbolinks2', to: 'turbolinks_test#page2', as: :turbolinks2
+      get '/turbolinks3', to: 'turbolinks_test#page3', as: :turbolinks3
+      post '/action_test', to: 'action_test#test'
+    end
     Rails.application.reload_routes!
-  end
 
-  specify "Matestack can be used with turbolinks" do
-    visit "/turbolinks"
+    class TurbolinksTestController < ActionController::Base
+      layout "application_with_turbolinks"
 
-    expect(page).to have_text "Hello from matestack without turbolinks"
+      include Matestack::Ui::Core::ApplicationHelper
+
+      def page1
+        responder_for(Pages::TurbolinksTest::Page1)
+      end
+      def page2
+        responder_for(Pages::TurbolinksTest::Page2)
+      end
+      def page3
+        responder_for(Pages::TurbolinksTest::Page3)
+      end
+    end
+
+    class ActionTestController < TestController
+
+      def test
+        render json: {}, status: 200
+      end
+
+    end
+    allow_any_instance_of(ActionTestController).to receive(:expect_params)
+
+
+    visit "/turbolinks1"
+
+    expect(page).to have_text "Hello from matestack with turbolinks - Page 1"
+
+    click_button "Transition to Page 2"
+
+    expect(page).to have_text "Hello from matestack with turbolinks - Page 2"
+
+    click_button "Link to Page 1"
+
+    expect(page).to have_text "Hello from matestack with turbolinks - Page 1"
+
+    click_button "Transition to Page 2"
+
+    expect(page).to have_text "Hello from matestack with turbolinks - Page 2"
+
+    click_button "Link to Page 1"
+
+    expect(page).to have_text "Hello from matestack with turbolinks - Page 1"
+
+    visit "/turbolinks3"
+
+    expect(page).to have_text "Hello from matestack with turbolinks - Page 3"
+
+    expect_any_instance_of(ActionTestController).to receive(:expect_params)
+      .with(hash_including(:foo => "bar"))
+
+    click_button "click me"
+
+    expect(page).to have_text "Hello from matestack with turbolinks - Page 3"
   end
 end
