bac1eefc67644e2829a0221ae48bb70ffe39df65
Nils Henning <nils@matestack.io>
2020-07-07 12:34:50 +0200

    update async component and specs

--- spec/0.8/components/dynamic/async/rerender_spec.rb
+++ spec/0.8/components/dynamic/async/rerender_spec.rb
@@ -4,9 +4,7 @@ include Utils
 describe "Async Component", type: :feature, js: true do
 
   it "rerender on event(s) on page-level" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         async rerender_on: "my_event" do
           div id: "my-div" do
@@ -23,38 +21,28 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
-
     end
 
     visit "/example"
-
     element = page.find("#my-div")
     before_content = element.text
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_event")')
-
     expect(page).not_to have_content(before_content)
-
     element = page.find("#my-second-div")
     before_content = element.text
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("multi_event_1")')
-
     expect(page).not_to have_content(before_content)
-
     element = page.find("#my-second-div")
     before_content = element.text
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("multi_event_2")')
-
     expect(page).not_to have_content(before_content)
   end
 
   it "rerender on event behind yield_components" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         div do
           some_component do
@@ -66,11 +54,9 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
     end
 
     class SomeComponent < Matestack::Ui::StaticComponent
-
       def response
         div id: "yielded-content-1" do
           yield_components
@@ -85,63 +71,46 @@ describe "Async Component", type: :feature, js: true do
       end
 
       register_self_as(:some_component)
-
     end
 
     class OtherComponent < Matestack::Ui::StaticComponent
-
       def response
-        components {
-          div id: "nested-yielded-content-2" do
-            yield_components
-          end
-        }
+        div id: "nested-yielded-content-2" do
+          yield_components
+        end
       end
 
       register_self_as(:other_component)
-
     end
 
 
     visit "/example"
-
     content_before = page.find("#yielded-async-1").text
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_event")')
-
-
     expect(page).not_to have_content(content_before)
-
     within "#yielded-content-1" do
       expect(page).to have_content("yielded timestamp 1: ")
     end
-
     content_before = page.find("#nested-yielded-async-2").text
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_other_event")')
-
     expect(page).not_to have_content(content_before)
-
     within "#nested-yielded-content-2" do
       expect(page).to have_content("yielded timestamp 2: ")
     end
-
   end
 
   it "rerender on event on (nested) component-level" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         div do
           some_component
         end
       end
-
     end
 
     class SomeComponent < Matestack::Ui::StaticComponent
-
       def response
         div id: "static-content-on-component" do
           plain "Component 1.1: #{DateTime.now.strftime('%Q')}"
@@ -164,91 +133,76 @@ describe "Async Component", type: :feature, js: true do
       end
 
       register_self_as(:some_component)
-
     end
 
     class OtherComponent < Matestack::Ui::StaticComponent
-
       def response
-        components {
-          div id: "static-content-on-other-component" do
-            plain "Component 2.1: #{DateTime.now.strftime('%Q')}"
+        div id: "static-content-on-other-component" do
+          plain "Component 2.1: #{DateTime.now.strftime('%Q')}"
+        end
+        async rerender_on: "my_event, some_other_event" do
+          div id: "dynamic-content-on-other-component" do
+            plain "Component 2.2: #{DateTime.now.strftime('%Q')}"
           end
-          async rerender_on: "my_event, some_other_event" do
-            div id: "dynamic-content-on-other-component" do
-              plain "Component 2.2: #{DateTime.now.strftime('%Q')}"
-            end
+        end
+        async rerender_on: "my_other_event" do
+          div id: "other-dynamic-content-on-other-component" do
+            plain "Component 2.3: #{DateTime.now.strftime('%Q')}"
           end
-          async rerender_on: "my_other_event" do
-            div id: "other-dynamic-content-on-other-component" do
-              plain "Component 2.3: #{DateTime.now.strftime('%Q')}"
-            end
-          end
-        }
+        end
       end
 
       register_self_as(:other_component)
-
     end
 
-
     visit "/example"
-    # sleep
-
     static_content_before = page.find("#static-content-on-component").text
     dynamic_content_before = page.find("#dynamic-content-on-component").text
     other_dynamic_content_before = page.find("#other-dynamic-content-on-component").text
-
     static_content_before_on_other_component = page.find("#static-content-on-other-component").text
     dynamic_content_before_on_other_component = page.find("#dynamic-content-on-other-component").text
     other_dynamic_content_before_on_other_component = page.find("#other-dynamic-content-on-other-component").text
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_event")')
-
     expect(page).to have_content(static_content_before)
     expect(page).not_to have_content(dynamic_content_before)
     expect(page).to have_content(other_dynamic_content_before)
-
     expect(page).to have_content(static_content_before_on_other_component)
     expect(page).not_to have_content(dynamic_content_before_on_other_component)
     expect(page).to have_content(other_dynamic_content_before_on_other_component)
-
     static_content_before = page.find("#static-content-on-component").text
     dynamic_content_before = page.find("#dynamic-content-on-component").text
     other_dynamic_content_before = page.find("#other-dynamic-content-on-component").text
-
     static_content_before_on_other_component = page.find("#static-content-on-other-component").text
     dynamic_content_before_on_other_component = page.find("#dynamic-content-on-other-component").text
     other_dynamic_content_before_on_other_component = page.find("#other-dynamic-content-on-other-component").text
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("some_other_event")')
-
     expect(page).to have_content(static_content_before)
     expect(page).to have_content(dynamic_content_before)
     expect(page).not_to have_content(other_dynamic_content_before)
-
     expect(page).not_to have_content(static_content_before_on_other_component)
     expect(page).not_to have_content(dynamic_content_before_on_other_component)
     expect(page).not_to have_content(other_dynamic_content_before_on_other_component)
   end
 
   it "rerender on event on page-level if wrapped in app" do
+    module Example
+    end
 
-    class Apps::ExampleApp < Matestack::Ui::App
-
+    class Example::App < Matestack::Ui::App
       def response
         heading size: 1, text: "My Example App Layout"
         main do
           page_content
         end
       end
-
     end
 
-    module Pages::ExampleApp end
+    module Example::Pages 
+    end
 
-    class Pages::ExampleApp::ExamplePage < Matestack::Ui::Page
-
+    class Example::Pages::ExamplePage < Matestack::Ui::Page
       def response
         async rerender_on: "my_event" do
           div id: "my-div" do
@@ -256,16 +210,14 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
     end
 
     class AsyncInAppPagesController < ExampleController
       include Matestack::Ui::Core::ApplicationHelper
 
       def example
-        render Pages::ExampleApp::ExamplePage, matestack_app: ExampleApp
+        render Example::Pages::ExamplePage, matestack_app: Example::App
       end
-
     end
 
     Rails.application.routes.append do
@@ -276,15 +228,12 @@ describe "Async Component", type: :feature, js: true do
     Rails.application.reload_routes!
 
     visit "async_rerender_spec/async_specs/in_app/example"
-
     element = page.find("#my-div")
     before_content = element.text
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_event")')
-
     element = page.find("#my-div")
     after_content = element.text
-
     expect(before_content).not_to eq(after_content)
   end
 
--- spec/0.8/components/dynamic/async/show_hide_spec.rb
+++ spec/0.8/components/dynamic/async/show_hide_spec.rb
@@ -4,9 +4,7 @@ include Utils
 describe "Async Component", type: :feature, js: true do
 
   it "show on event" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         async show_on: "my_event" do
           div id: "my-div" do
@@ -19,37 +17,29 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
     end
 
     visit "/example"
-
     expect(page).not_to have_selector "#my-div"
     expect(page).not_to have_selector "#my-second-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_event")')
-
     expect(page).to have_selector "#my-div"
     expect(page).not_to have_selector "#my-second-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("multi_event_2")')
-
     expect(page).to have_selector "#my-div"
     expect(page).to have_selector "#my-second-div"
 
     visit "/example"
-
     expect(page).not_to have_selector "#my-second-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("multi_event_1")')
-
     expect(page).to have_selector "#my-second-div"
   end
 
   it "hide on event" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         async hide_on: "my_event" do
           div id: "my-div" do
@@ -62,37 +52,29 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
     end
 
     visit "/example"
-
     expect(page).to have_selector "#my-div"
     expect(page).to have_selector "#my-second-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_event")')
-
     expect(page).not_to have_selector "#my-div"
     expect(page).to have_selector "#my-second-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("multi_event_2")')
-
     expect(page).not_to have_selector "#my-div"
     expect(page).not_to have_selector "#my-second-div"
 
     visit "/example"
-
     expect(page).to have_selector "#my-second-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("multi_event_1")')
-
     expect(page).not_to have_selector "#my-second-div"
   end
 
   it "show on / hide on combination init not shown by default" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         async show_on: "my_show_event", hide_on: "my_hide_event" do
           div id: "my-div" do
@@ -100,26 +82,20 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
     end
 
     visit "/example"
-
     expect(page).not_to have_selector "#my-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_show_event")')
-
     expect(page).to have_selector "#my-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_hide_event")')
-
     expect(page).not_to have_selector "#my-div"
   end
 
   it "show on / hide on combination init shown if configured" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         async show_on: "my_show_event", hide_on: "my_hide_event", init_show: true do
           div id: "my-div" do
@@ -127,26 +103,20 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
     end
 
     visit "/example"
-
     expect(page).to have_selector "#my-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_hide_event")')
-
     expect(page).not_to have_selector "#my-div"
 
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_show_event")')
-
     expect(page).to have_selector "#my-div"
   end
 
   it "hide after show on event" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         async show_on: "my_event", hide_after: 1000 do
           div id: "my-div" do
@@ -154,23 +124,20 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
     end
 
     visit "/example"
-
     expect(page).not_to have_selector "#my-div"
+
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_event")')
     expect(page).to have_selector "#my-div"
+
     sleep 1
     expect(page).not_to have_selector "#my-div"
-
   end
 
   it "show on event with event payload" do
-
     class ExamplePage < Matestack::Ui::Page
-
       def response
         async show_on: "my_event" do
           div id: "my-div" do
@@ -178,16 +145,13 @@ describe "Async Component", type: :feature, js: true do
           end
         end
       end
-
     end
 
     visit "/example"
-
     expect(page).not_to have_content "test!"
+
     page.execute_script('MatestackUiCore.matestackEventHub.$emit("my_event", { message: "test!" })')
     expect(page).to have_content "test!"
-
   end
-
 
 end
