b4b83a1848196de1c4f10bd0a6ac0a4e79547ca3
Meagan Butters <meagan.butters@hotmail.com>
2021-11-08 15:16:24 -0500

    logs/console

--- lib/matestack/ui/vue_js/components/toggle.rb
+++ lib/matestack/ui/vue_js/components/toggle.rb
@@ -4,24 +4,26 @@ module Matestack
       module Components
         class Toggle < Matestack::Ui::VueJs::Vue
           vue_name 'matestack-ui-core-toggle'
-
+ 
           optional :show_on, :hide_on, :hide_after, :init_show
-
+ 
           def response
             div toggle_attributes do
               yield
             end
           end
-
+ 
           def toggle_attributes
-            options.merge({
-              class: "matestack-toggle-component-root", 
-              'v-if': 'showing'
-            })
+            a = options.merge({
+                                class: "matestack-toggle-component-root",
+                                'v-if': 'showing'
+                              })
+            puts("**** #{a} !!!!")
+            a
           end
-
+ 
           protected
-
+ 
           def vue_props
             {
               show_on: ctx.show_on,
@@ -30,9 +32,10 @@ module Matestack
               init_show: ctx.init_show,
             }
           end
-
+ 
         end
       end
     end
   end
-end
+ end
+ 
